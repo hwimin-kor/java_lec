a4f5a142b3c602105b3ca07eb7c9c1442dd527a5
hwimin-kor <758gnl@naver.com>
2024-11-04 13:15:19 +0900

    A

---
+++ 01_start/src/start/Start.java
@@ -0,0 +1,25 @@
+package start;
+
+/**
+ * Documentation comment(class) 클래스 설명하는 주석 클래스 위에 적는다 
+ * 작성자, 작성일자, 버전, 설명 등을 API 문서로 제공하기 위한 주석 
+ */
+public class Start {
+/**
+ * Documentation comment (method)메소드에 대한 설명을 적어주는 주석 
+ * @param args
+ */
+  public static void main(String[] args) {
+    
+    //single-line comment
+    
+    /*
+     * multi-line comment
+     */
+    //console 출력 방식 
+    System.out.print(1);        //1출력
+    System.out.println(2);      //2출력 후 줄바꿈
+    System.out.printf("%d\n",3);//3출력 후 줄 바꿈 
+  }
+
+}
